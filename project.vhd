eb68598cb2c159d183fc874a2d04be1684a80344
jojorojo1 <59421205+jojorojo1@users.noreply.github.com>
2022-03-26 20:59:05 +0000

    Add files via upload

--- project.vhd
+++ project.vhd
@@ -42,7 +42,8 @@ entity project is
            bday3 : in STD_LOGIC;
            led : out STD_LOGIC_VECTOR (15 downto 0);
            anode : out STD_LOGIC_VECTOR (3 downto 0);
-           seven : out STD_LOGIC_VECTOR (6 downto 0));
+           seven : out STD_LOGIC_VECTOR (6 downto 0);
+           db : out STD_LOGIC);
          
 end project;
 
@@ -86,16 +87,19 @@ clk_divide : process (clk_in) is
   
   begin
   
+    db <= '0';
+  
     if rising_edge(clk) then
         
         if (reset = '1') then
         
             count := "0000";
             
+            
          elsif (enable = '1') then
             
             count := count + 1;
-            
+          
           end if;
           
        end if;
@@ -113,99 +117,83 @@ clk_divide : process (clk_in) is
       
         button := "11";
         
+      else 
+      
+       button := "00";
+      
       end if;
-        
-       
+      
       case to_integer(button) is
       
-        when 1 =>
-       
-                case to_integer(count) is 
-                 when 0=> 
-                    anode<="0111";
-                    seven<="0010010";
-                 when 1=> 
-                    anode<="1011";
-                    seven<="0000100";
-                when 2=> 
-                    anode<="1101";
-                    seven<="1001111";
-                when 3=> 
-                    anode<="1110";
-                    seven<="1001111";
-                when others=>
-                    anode<="1111";
-                    seven<="1111111";
-                end case;
-
+        when 1 => --pavlos led
         
             case to_integer(count) is 
         
                 when 0 =>
     
-                 led <= "1111111100000000";
+                 led <= "1111110011111111";
         
                 when 1 =>
         
      
-                    led <= "1000011001100001";
+                    led <= "1000000001100001";
   
                  when 2 =>
         
       
-                    led <= "0000011001100000";
+                    led <= "0000111100110000";
   
                 when 3=>
                 
-                    led <= "0001100000011000";
+                    led <= "0001100110011000";
    
                 when 4=>
                 
-                    led <= "0011000000001100";
+                    led <= "0011001100001100";
    
                 when 5=>
                 
-                    led <= "0110000000000110";
+                    led <= "0110000000011110";
    
                 when 6=>
                 
-                    led <= "1100000000000011"; 
+                    led <= "1100001100000011"; 
                 
                 when 7=>
                 
-                    led <= "1010000000000101"; 
+                    led <= "1010000000001101"; 
                   
                 when 8=>
                 
-                    led <= "0101000000001010";  
-                  
+                    led <= "1101000000011010";
+
                 when 9=>
                 
-                    led <= "0010100000010100";
+                    led <= "0010100000110100";
                 
                 when 10=>
                 
-                    led <= "0001010000101000"; 
+                    led <= "0111010000101000"; 
                  
                 when 11=>
                 
-                    led <= "0000101001010000";
+                    led <= "0000111001010000";
                 
                 when 12=>
                 
-                    led <= "0000010110100000";
+                    led <= "0000010110110000";
                 
                 when 13=>
                 
-                    led <= "0000001111000000";
+                    led <= "0000011111000000";
                 
                 when 14=>
                 
-                    led <= "0000001111000000";
+                    led <= "0000000110000000";
                 
                 when 15=>
                 
-                    led <= "0000010110100000";
+                    led <= "0000011110100000";
    
                 when others =>
       
@@ -214,26 +202,7 @@ clk_divide : process (clk_in) is
                 end case;
                 
          when 2 =>
-                  
-              case to_integer(count) is 
-                when 0=> 
-                    anode<="0111";
-                    seven<="0010010";
-                when 1=> 
-                    anode<="1011";
-                    seven<="0000100";
-                when 2=> 
-                    anode<="1101";
-                    seven<="1001111";
-                when 3=> 
-                    anode<="1110";
-                    seven<="1001111";
-                when others=>
-                    anode<="1111";
-                    seven<="1111111";
-                end case;
-  
-               
+                
                 case to_integer(count) is 
     
                  when 0 =>
@@ -252,7 +221,7 @@ clk_divide : process (clk_in) is
   
                 when 3=>
                 
-                led <= "000100000001000";
+                led <= "0001000000001000";
    
                 when 4=>
                 led <= "0000100000010000";
@@ -289,73 +258,53 @@ clk_divide : process (clk_in) is
                 end case;
             
            when 3 =>
-           
-           
-           case to_integer(count) is 
-                when 0=> 
-                    anode<="0111";
-                    seven<="0010010";
-                when 1=> 
-                    anode<="1011";
-                    seven<="0000100";
-                 when 2=> 
-                   anode<="1101";
-                   seven<="1001111";
-                when 3=> 
-                    anode<="1110";
-                    seven<="1001111";
-                when others=>
-                    anode<="1111";
-                    seven<="1111111";
-                end case;
-
                     
                   case to_integer(count) is 
     
                                when 0 =>
     
-                 led <= "1000111111110001";
+                 led <= "1000000000000000";
         
                 when 1 =>
         
      
-                led <= "00000000000000";
+                led <= "0000000000000001";
   
                  when 2 =>
         
       
-                 led <= "0000011001100000";
+                 led <= "0000000000000010";
   
                 when 3=>
                 
-                led <= "0001100000011000";
+                led <= "0000000000000100";
    
                 when 4=>
-                led <= "0011000000001100";
+                led <= "0000000000001000";
    
                 when 5=>
-                led <= "0110000000000110";
+                led <= "0000000000010000";
    
                 when 6=>
-                led <= "1100000000000011"; 
+                led <= "0000000000100000"; 
                 when 7=>
-                led <= "1010000000000101";   
+                led <= "0000000001000000";   
                 when 8=>
-                led <= "0101000000001010";    
+                led <= "0000000010000000";    
                 when 9=>
-                led <= "0010100000010100";
+                led <= "0000000100000000";
                 when 10=>
-                led <= "0001010000101000";  
+                led <= "0000001000000000";  
                 when 11=>
-                led <= "0000101001010000";
+                led <= "0000010000000000";
                 when 12=>
-                led <= "0000010110100000";
+                led <= "0000100000000000";
                 when 13=>
-                led <= "0000001111000000";
+                led <= "0001000000000000";
                 when 14=>
-                led <= "0000001111000000";
+                led <= "0010000000000000";
                 when 15=>
-                led <= "0000010110100000";
+                led <= "0100000000000000";
                  
                   when others =>
            
@@ -375,30 +324,154 @@ clk_divide : process (clk_in) is
    end process;
    
    
-   end Behavioral;          
-
-            
+   sevenseg : process (clk_in) is
+   
+    variable count : unsigned(3 downto 0) := "0000";
+  
+    variable button : unsigned(1 downto 0) := "00";
+     
+    begin
+     
+     if rising_edge(clk_in) then
+     
+        count := count + 1;
+        
+     end if;
+     
+     
+      if (bday1 = '1') then
+           
+          button := "01";
+          
+      elsif (bday2 = '1') then
        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+         button := "10";
+         
+      elsif  (bday3 = '1') then
+      
+        button := "11";
+        
+      else 
+      
+       button := "00";
+      
+      end if;
+       
+       
+       
+        
+     case to_integer(button) is
+      
+      when 1 =>
+      
+        case to_integer(count) is 
+                 when 0=> 
+                    anode<="0111";
+                    seven<="0010010";
+                 when 1=> 
+                    anode<="1011";
+                    seven<="0000100";
+                when 2=> 
+                    anode<="1101";
+                    seven<="1001111";
+                when 3=> 
+                    anode<="1110";
+                    seven<="1001111";
+                when others=>
+                    anode<="1111";
+                    seven<="1111111";
+                end case;
+      
+      when 2 =>
+      
+        case to_integer(count) is 
+                when 0=> 
+                    anode<="0111";
+                    seven<="0010010";
+                when 1=> 
+                    anode<="1011";
+                    seven<="0000001";
+                when 2=> 
+                    anode<="1101";
+                    seven<="0000001";
+                when 3=> 
+                    anode<="1110";
+                    seven<="1001111";
+                 when 4=> 
+                    anode<="0111";
+                    seven<="0010010";
+                 when 5=> 
+                    anode<="1011";
+                    seven<="0000100";
+                when 6=> 
+                    anode<="1101";
+                    seven<="1001111";
+                when 7=> 
+                    anode<="1110";
+                    seven<="1001111";
+                    when 8=> 
+                    anode<="0111";
+                    seven<="0010010";
+                 when 9=> 
+                    anode<="1011";
+                    seven<="0000100";
+                when 10=> 
+                    anode<="1101";
+                    seven<="1001111";
+                when 11=> 
+                    anode<="1110";
+                    seven<="1001111";
+                    when 12=> 
+                    anode<="0111";
+                    seven<="0010010";
+                 when 13=> 
+                    anode<="1011";
+                    seven<="0000100";
+                when 14=> 
+                    anode<="1101";
+                    seven<="1001111";
+                when 15=> 
+                    anode<="1110";
+                    seven<="1001111";
+                when others=>
+                    anode<="1111";
+                    seven<="1111111";
+     
+                end case;
+                
+       when 3 =>
+       
+        case to_integer(count) is 
+                when 0=> 
+                    anode<="0111";
+                    seven<="0000001";
+                when 1=> 
+                    anode<="1011";
+                    seven<="0000110";
+                 when 2=> 
+                   anode<="1101";
+                   seven<="0000001";
+                when 3=> 
+                    anode<="1110";
+                    seven<="0000110";
+                when others=>
+                    anode<="1111";
+                    seven<="1111111";
+                end case;
+                
+       when others =>
+       
+          anode <= "1111";
+          seven <= "1111111";
+          
+   end case;
+   
+   end process;
+   
+   
+   
+   
+   
+   
+   
+   end Behavioral;          
